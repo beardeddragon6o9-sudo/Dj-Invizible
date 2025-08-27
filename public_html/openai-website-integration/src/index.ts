9579e6bb5490a99db6b4cf8716fa24bb062681fe
beardeddragon6o9-sudo <beardeddragon6o9@gmail.com>
2025-08-27 23:11:33 +0000

    Move site to root; delete public_html

--- public_html/openai-website-integration/src/index.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Home from './pages/Home';
-
-const App = () => {
-    return <Home />;
-};
-
-ReactDOM.render(<App />, document.getElementById('root'));
