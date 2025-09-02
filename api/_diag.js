41e670542c5694b06865fefbb71a821c6f027fc3
beardeddragon6o9-sudo <beardeddragon6o9@gmail.com>
2025-09-02 16:53:19 -0700

    diag: show which Google env vars are set

---
+++ api/_diag.js
@@ -0,0 +1,15 @@
+﻿module.exports = async (req, res) => {
+  try {
+    const keys = [
+      "GOOGLE_CLIENT_ID",
+      "GOOGLE_CLIENT_SECRET",
+      "GOOGLE_REDIRECT_URI",
+      "GOOGLE_REFRESH_TOKEN",
+      "SWEEP_SECRET"
+    ];
+    const present = Object.fromEntries(keys.map(k => [k, !!process.env[k]]));
+    return res.status(200).json({ ok:true, present });
+  } catch (e) {
+    return res.status(500).json({ ok:false, error:String(e) });
+  }
+};
